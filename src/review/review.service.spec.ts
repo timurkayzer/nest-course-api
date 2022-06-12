fc84811f7468813576cdbaa19ead4c119eb9e85e
Timur <timurkayzer@gmail.com>
2022-06-13 00:30:13 +0500

    unit tests

--- src/review/review.service.spec.ts
+++ src/review/review.service.spec.ts
@@ -1,12 +1,23 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
 import { ReviewService } from './review.service';
 
 describe('ReviewService', () => {
   let service: ReviewService;
 
+  const reviewRepositoryFactory = () => {
+    return {
+      find: jest.fn()
+    };
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewService],
+      providers: [
+        ReviewService,
+        { useFactory: reviewRepositoryFactory, provide: getModelToken('ReviewModel') }
+      ],
     }).compile();
 
     service = module.get<ReviewService>(ReviewService);
@@ -14,5 +25,12 @@ describe('ReviewService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findByProductId', async () => {
+    const id = new Types.ObjectId().toHexString();
+    reviewRepositoryFactory().find.mockReturnValueOnce([{ productId: id }]);
+    const res = await service.findByProductId(id);
+    expect(res[0].productId).toBe(id);
   });
 });
